bd31cbbd012f5f857520e7e75579216e05f7376c
kat1248 <kat1248@gmail.com>
2018-02-02 15:14:12 -0500

    changed names

--- gce/create.sh
+++ gce/create.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-gcloud compute instances create my-app-instance \
+gcloud compute instances create sclh-instance \
        --image-family=debian-8 \
        --image-project=debian-cloud \
-       --machine-type=g1-small \
+       --machine-type=f1-micro \
        --scopes userinfo-email,cloud-platform \
        --metadata-from-file startup-script=gce/startup-script.sh \
-       --zone us-central1-f \
+       --zone us-east1-b \
        --tags http-server
