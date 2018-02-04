be0acf7010456acb7874f165eaf76f70b9e6d2c4
kat1248 <kat1248@gmail.com>
2018-02-04 15:33:28 -0500

    removed date code

--- character.go
+++ character.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
@@ -532,50 +531,4 @@ func fetchRecentKillHistory(id int) *CharacterResponse {
 	cd.KillsLastWeek = len(entries)
 
 	return &CharacterResponse{&cd, nil}
-}
-
-const (
-	SecondsInMinute = 60
-	SecondsInHour   = 60 * SecondsInMinute
-	SecondsInDay    = 24 * SecondsInHour
-	SecondsInMonth  = 30 * SecondsInDay
-	SecondsInYear   = 365 * SecondsInDay
-)
-
-func secondsToTimeString(seconds float64) string {
-	s := int(seconds)
-	years := s / SecondsInYear
-	s -= years * SecondsInYear
-	months := s / SecondsInMonth
-	s -= months * SecondsInMonth
-	days := s / SecondsInDay
-	ts := ""
-	if years > 0 {
-		ts += fmt.Sprint(years) + "y"
-	}
-	if months > 0 {
-		ts += fmt.Sprint(months) + "m"
-	}
-	if days > 0 {
-		ts += fmt.Sprint(days) + "d"
-	}
-	if ts == "" {
-		ts = "today"
-	}
-	return ts
-}
-
-func secondsSince(dt string) float64 {
-	t, _ := time.Parse("2006-01-02T15:04:05Z", dt)
-	duration := time.Since(t)
-
-	return duration.Seconds()
-}
-
-func daysSince(dt string) int {
-	return int(secondsSince(dt)) / 86400
-}
-
-func getDate(dt string) string {
-	return strings.Split(dt, "T")[0]
 }
